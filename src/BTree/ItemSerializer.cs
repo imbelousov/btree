1bc77eba4bbc39a19748d21fb294065e5dee26b8
Igor Belousov <belousov.i@skbkontur.ru>
2020-11-06 19:16:11 +0500

    Some implementations serializer

--- src/BTree/ItemSerializer.cs
+++ src/BTree/ItemSerializer.cs
@@ -74,6 +74,51 @@ namespace BTree
 		public byte DeserializeItem(ReadOnlySpan<byte> buffer) => buffer[0];
 	}
 
+	internal class SByteSerializer : IItemSerializer<sbyte>
+	{
+		public int MaxSerializedItemLength => 1;
+
+		public void SerializeItem(sbyte item, Span<byte> buffer) => buffer[0] = (byte)item;
+
+		public sbyte DeserializeItem(ReadOnlySpan<byte> buffer) => (sbyte)buffer[0];
+	}
+
+	internal class BooleanSerializer : IItemSerializer<bool>
+	{
+		public int MaxSerializedItemLength => 1;
+
+		public void SerializeItem(bool item, Span<byte> buffer) => buffer[0] = (byte)(item ? 1 : 0);
+
+		public bool DeserializeItem(ReadOnlySpan<byte> buffer) => buffer[0] != 0;
+	}
+
+	internal class Int16Serializer : IItemSerializer<short>
+	{
+		public int MaxSerializedItemLength => sizeof(short);
+
+		public void SerializeItem(short item, Span<byte> buffer) => BitConverter.TryWriteBytes(buffer, item);
+
+		public short DeserializeItem(ReadOnlySpan<byte> buffer) => BitConverter.ToInt16(buffer);
+	}
+
+	internal class UInt16Serializer : IItemSerializer<ushort>
+	{
+		public int MaxSerializedItemLength => sizeof(ushort);
+
+		public void SerializeItem(ushort item, Span<byte> buffer) => BitConverter.TryWriteBytes(buffer, item);
+
+		public ushort DeserializeItem(ReadOnlySpan<byte> buffer) => BitConverter.ToUInt16(buffer);
+	}
+
+	internal class CharSerializer : IItemSerializer<char>
+	{
+		public int MaxSerializedItemLength => sizeof(char);
+
+		public void SerializeItem(char item, Span<byte> buffer) => BitConverter.TryWriteBytes(buffer, item);
+
+		public char DeserializeItem(ReadOnlySpan<byte> buffer) => BitConverter.ToChar(buffer);
+	}
+
 	internal class Int32Serializer : IItemSerializer<int>
 	{
 		public int MaxSerializedItemLength => sizeof(int);
@@ -81,5 +126,75 @@ namespace BTree
 		public void SerializeItem(int item, Span<byte> buffer) => BitConverter.TryWriteBytes(buffer, item);
 
 		public int DeserializeItem(ReadOnlySpan<byte> buffer) => BitConverter.ToInt32(buffer);
+	}
+
+	internal class UInt32Serializer : IItemSerializer<uint>
+	{
+		public int MaxSerializedItemLength => sizeof(uint);
+
+		public void SerializeItem(uint item, Span<byte> buffer) => BitConverter.TryWriteBytes(buffer, item);
+
+		public uint DeserializeItem(ReadOnlySpan<byte> buffer) => BitConverter.ToUInt32(buffer);
+	}
+
+	internal class Int64Serializer : IItemSerializer<long>
+	{
+		public int MaxSerializedItemLength => sizeof(long);
+
+		public void SerializeItem(long item, Span<byte> buffer) => BitConverter.TryWriteBytes(buffer, item);
+
+		public long DeserializeItem(ReadOnlySpan<byte> buffer) => BitConverter.ToInt64(buffer);
+	}
+
+	internal class UInt64Serializer : IItemSerializer<ulong>
+	{
+		public int MaxSerializedItemLength => sizeof(ulong);
+
+		public void SerializeItem(ulong item, Span<byte> buffer) => BitConverter.TryWriteBytes(buffer, item);
+
+		public ulong DeserializeItem(ReadOnlySpan<byte> buffer) => BitConverter.ToUInt64(buffer);
+	}
+
+	internal class SingleSerializer : IItemSerializer<float>
+	{
+		public int MaxSerializedItemLength => sizeof(float);
+
+		public void SerializeItem(float item, Span<byte> buffer) => BitConverter.TryWriteBytes(buffer, item);
+
+		public float DeserializeItem(ReadOnlySpan<byte> buffer) => BitConverter.ToSingle(buffer);
+	}
+
+	internal class DoubleSerializer : IItemSerializer<double>
+	{
+		public int MaxSerializedItemLength => sizeof(double);
+
+		public void SerializeItem(double item, Span<byte> buffer) => BitConverter.TryWriteBytes(buffer, item);
+
+		public double DeserializeItem(ReadOnlySpan<byte> buffer) => BitConverter.ToDouble(buffer);
+	}
+
+	internal class DecimalSerializer : IItemSerializer<decimal>
+	{
+		public int MaxSerializedItemLength => sizeof(decimal);
+
+		public unsafe void SerializeItem(decimal item, Span<byte> buffer)
+		{
+			var ptr = &item;
+			var l1 = *(long*)ptr;
+			var l2 = *((long*)ptr + 1);
+			BitConverter.TryWriteBytes(buffer, l1);
+			BitConverter.TryWriteBytes(buffer.Slice(sizeof(long)), l2);
+		}
+
+		public unsafe decimal DeserializeItem(ReadOnlySpan<byte> buffer)
+		{
+			var result = new decimal();
+			var l1 = BitConverter.ToInt64(buffer);
+			var l2 = BitConverter.ToInt64(buffer.Slice(sizeof(long)));
+			var ptr = &result;
+			*(long*)ptr = l1;
+			*((long*)ptr + 1) = l2;
+			return result;
+		}
 	}
 }
