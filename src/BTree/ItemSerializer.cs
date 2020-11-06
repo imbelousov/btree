5439be6d7798ce51c63608d684bebffb955cc017
Igor Belousov <belousov.i@skbkontur.ru>
2020-11-06 16:16:45 +0500

    Default serializer

--- src/BTree/ItemSerializer.cs
+++ src/BTree/ItemSerializer.cs
@@ -1,72 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BTree
 {
-    public interface IItemSerializer<T>
-    {
-        int MaxSerializedItemLength { get; }
+	public interface IItemSerializer<T>
+	{
+		int MaxSerializedItemLength { get; }
 
-        void SerializeItem(T item, Span<byte> buffer);
+		void SerializeItem(T item, Span<byte> buffer);
 
-        T DeserializeItem(ReadOnlySpan<byte> buffer);
-    }
+		T DeserializeItem(ReadOnlySpan<byte> buffer);
+	}
 
-    public sealed class ItemSerializer<T> : IItemSerializer<T>
-    {
-        public static readonly ItemSerializer<T> Default = new ItemSerializer<T>();
+	internal static class TypedSerializers
+	{
+		private static readonly Dictionary<Type, object> Instances;
 
-        private static readonly Dictionary<Type, DefaultSerializerInfo<T>> SupportedTypes = new Dictionary<Type, DefaultSerializerInfo<T>>
-        {
-            {typeof(byte), new DefaultSerializerInfo<T>(sizeof(byte), SerializeByte,)}
-        };
+		static TypedSerializers()
+		{
+			Instances = FindTypedSerializers().ToDictionary(GetSerializedType, Activator.CreateInstance);
+		}
 
-        private readonly SerializeItem<T> _serializeItem;
-        private readonly DeserializeItem<T> _deserializeItem;
+		public static bool TryGet<T>(out IItemSerializer<T> serializer)
+		{
+			if(!Instances.TryGetValue(typeof(T), out var obj))
+			{
+				serializer = default;
+				return false;
+			}
+			serializer = (IItemSerializer<T>)obj;
+			return true;
+		}
 
-        private ItemSerializer()
-        {
-            if (!SupportedTypes.TryGetValue(typeof(T), out var info))
-                throw new NotSupportedException($"Type '{typeof(T)}' is not supported. You have to implement '{nameof(IItemSerializer<T>)}' manually.");
-            MaxSerializedItemLength = info.MaxSerializedItemLength;
-            _serializeItem = info.SerializeItem;
-            _deserializeItem = info.DeserializeItem;
-        }
+		private static IEnumerable<Type> FindTypedSerializers() => typeof(IItemSerializer<>).Assembly
+			.GetTypes()
+			.Where(x => !x.IsAbstract && !x.IsGenericType)
+			.Where(x => x.GetImplementedInterface() != null);
 
-        public int MaxSerializedItemLength { get; }
+		private static Type GetSerializedType(Type serializerType) => serializerType
+			.GetImplementedInterface()
+			.GetGenericArguments()[0];
 
-        public void SerializeItem(T item, Span<byte> buffer) => _serializeItem(item, buffer);
+		private static Type GetImplementedInterface(this Type type) => type.GetInterfaces()
+			.FirstOrDefault(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IItemSerializer<>));
+	}
 
-        public T DeserializeItem(ReadOnlySpan<byte> buffer) => _deserializeItem(buffer);
+	public sealed class ItemSerializer<T> : IItemSerializer<T>
+	{
+		public static readonly ItemSerializer<T> Default = new ItemSerializer<T>();
 
-        private static void SerializeByte(T item, Span<byte> buffer)
-        {
-#pragma warning disable CS8509
-            buffer[0] = item switch {byte b => b};
-#pragma warning restore CS8509
-        }
+		private readonly IItemSerializer<T> _typedSerializer;
 
-        private static T DeserializeByte(Span<byte> buffer)
-        {
-            return (T) buffer[0];
-        }
-    }
+		public int MaxSerializedItemLength => _typedSerializer.MaxSerializedItemLength;
 
-    internal delegate void SerializeItem<T>(T item, Span<byte> buffer);
+		private ItemSerializer()
+		{
+			if(!TypedSerializers.TryGet<T>(out _typedSerializer))
+				throw new NotSupportedException($"Type '{typeof(T)}' is not supported. You have to implement '{nameof(IItemSerializer<T>)}' manually.");
+		}
 
-    internal delegate T DeserializeItem<T>(ReadOnlySpan<byte> buffer);
+		public void SerializeItem(T item, Span<byte> buffer) => _typedSerializer.SerializeItem(item, buffer);
 
-    internal struct DefaultSerializerInfo<T>
-    {
-        public int MaxSerializedItemLength { get; }
-        public SerializeItem<T> SerializeItem { get; }
-        public DeserializeItem<T> DeserializeItem { get; }
+		public T DeserializeItem(ReadOnlySpan<byte> buffer) => _typedSerializer.DeserializeItem(buffer);
+	}
 
-        public DefaultSerializerInfo(int maxSerializedItemLength, SerializeItem<T> serializeItem, DeserializeItem<T> deserializeItem)
-        {
-            MaxSerializedItemLength = maxSerializedItemLength;
-            SerializeItem = serializeItem;
-            DeserializeItem = deserializeItem;
-        }
-    }
+	internal class ByteSerializer : IItemSerializer<byte>
+	{
+		public int MaxSerializedItemLength => 1;
+
+		public void SerializeItem(byte item, Span<byte> buffer) => buffer[0] = item;
+
+		public byte DeserializeItem(ReadOnlySpan<byte> buffer) => buffer[0];
+	}
+
+	internal class Int32Serializer : IItemSerializer<int>
+	{
+		public int MaxSerializedItemLength => sizeof(int);
+
+		public void SerializeItem(int item, Span<byte> buffer) => BitConverter.TryWriteBytes(buffer, item);
+
+		public int DeserializeItem(ReadOnlySpan<byte> buffer) => BitConverter.ToInt32(buffer);
+	}
 }
